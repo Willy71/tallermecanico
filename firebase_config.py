4feb983cf5b4473675781e60c7209bded60e3cc1
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-30 10:57:09 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,12 +1,14 @@
 # firebase_config.py
-
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
+import os
+import json
 
-# Solo inicializar si aún no fue hecho
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    # Cargar desde variable de entorno
+    cred_dict = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-auth_admin = auth  # acceso al módulo de autenticación
+auth_admin = auth
